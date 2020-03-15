93202f3ef99139cfeec94b77e97deafe1bbccffa
David <info@nesiweb.com>
2020-03-15 15:34:45 +0100

    Fixes in Help Requests

--- app/Http/Controllers/Api/HelpRequest/HelpRequestController.php
+++ app/Http/Controllers/Api/HelpRequest/HelpRequestController.php
@@ -24,13 +24,13 @@ class HelpRequestController extends ApiController
 
         switch ($user->user_type) {
             case UserType::USER_TYPE_REQUESTER:
-                return HelpRequest::where('user_id', '=', Auth::id());
+                return HelpRequest::where('user_id', '=', Auth::id())->get();
 
             case UserType::USER_TYPE_VOLUNTEER:
-                return HelpRequest::whereNull('assigned_user_id');
+                return HelpRequest::where('assigned_user_id')->get();;
 
             default:
-                return response('', 400);
+                return response('Tipo de usuario no definido', 400);
         }
     }
 
@@ -52,12 +52,18 @@ class HelpRequestController extends ApiController
         if ($check) {
             return response('', 200);
         } else {
-            return response('', 400);
+            return response('Error al insertar', 400);
         }
     }
 
     public function put(Request $request)
     {
+        $user = Auth::user();
+
+        if ($user->user_type != UserType::USER_TYPE_VOLUNTEER) {
+            return response('Tipo de usuario no autorizado para esta acción', 403);
+        }
+
         request()->validate([
             'help_request_id' => 'required',
         ]);
@@ -65,13 +71,18 @@ class HelpRequestController extends ApiController
         $data = $request->all();
 
         $help_request = HelpRequest::find($data['help_request_id']);
+
+        if (!empty($help_request->assigned_user_id)) {
+            return response('Petición ya atendida con anterioridad', 400);
+        }
+
         $help_request->assigned_user_id = Auth::id();
         $help_request->accepted_at = time();
 
         if ($help_request->save()) {
             return response('', 200);
         } else {
-            return response('', 400);
+            return response('Error al actualizar', 400);
         }
     }
 }
