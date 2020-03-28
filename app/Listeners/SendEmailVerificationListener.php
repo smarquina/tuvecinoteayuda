c0c153a0a601ee3c861e47ffcdc5ccd34ff82698
David <info@nesiweb.com>
2020-03-28 21:55:48 +0100

    Fix email verification event

--- app/Listeners/SendEmailVerificationListener.php
+++ app/Listeners/SendEmailVerificationListener.php
@@ -12,6 +12,16 @@ class SendEmailVerificationListener {
      * @param Registered $event
      * @return void
      */
+    public function handle(Registered $event) {
+        $this->handleRegistered($event);
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param Registered $event
+     * @return void
+     */
     public function handleRegistered(Registered $event) {
         if (!$event->user->hasVerifiedEmail()) {
             $event->user->sendEmailVerificationNotification();
